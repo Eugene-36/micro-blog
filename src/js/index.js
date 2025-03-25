d0724f43ff4a312534887fb5935dc99827e1ef18
Eugene-36 <livenskiyjenya@gmail.com>
2025-03-25 22:20:50 +0200

    dark-mode

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,16 @@ import { Fancybox } from '@fancyapps/ui';
 import '@fancyapps/ui/dist/fancybox/fancybox.css';
 
 Fancybox.bind('[data-fancybox]', {
-	// Your custom options
+  // Your custom options
 });
 
 // Мобильная навигация
 import mobileNav from './modules/mobile-nav.js';
 mobileNav();
+
+const btnElement = document.getElementById('toggleDarkMode');
+btnElement.addEventListener('click', () => {
+  return document.body.classList.toggle('dark')
+    ? (btnElement.innerText = '☀️')
+    : (btnElement.innerText = '🌘');
+});
